f72b684687fdc5c65e5e0211600ec66d3dee5275
Benny <wonsang222@naver.com>
2025-02-27 20:12:34 +0900

    add CustomGraphView

--- practice/practice/BarGraphView.swift
+++ practice/practice/BarGraphView.swift
@@ -28,7 +28,7 @@ class BarGraphView: UIView {
         
     }
     
-    required init?(coder: NSCoder) {
+    required init?(coder: NSCoder) {    
         super.init(coder: coder)
         
         setupBars()
